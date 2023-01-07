ef5df9f5d65ae1513227770c62936b67fdc73506
Ben Graham <bengraham@Bens-MacBook-Air.local>
2023-01-07 21:22:16 +0200

    React Template 🦄

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import {
+  createBrowserRouter,
+  BrowserRouter, 
+  Route, 
+  createRoutesFromElements,
+  RouterProvider
+} from 'react-router-dom'
+
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
